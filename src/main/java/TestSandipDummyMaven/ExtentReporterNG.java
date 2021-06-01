22f62c0842aac08fd9ffdc9fa08b4cd08ce9cc2f
sandip <sandipdatta94@gmail.com>
2021-06-02 00:45:15 +0530

    GitX data commit to sandbox

---
+++ src/main/java/TestSandipDummyMaven/ExtentReporterNG.java
@@ -0,0 +1,9 @@
+package TestSandipDummyMaven;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterNG 
+{
+	
+	
+}
